a30a8f87a8abbcca3f992d7e75c73b9d32cd9a06
marwanviper <marwanabbas418@gmail.com>
2026-05-18 16:52:10 +0300

    add sync in exec

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,7 +2,18 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+#include "threads/synch.h" // <-- for semaphore
+/* Process management. */
+struct exec_info
+{
+    char *file_name;
 
+    struct semaphore load_sema;
+
+    bool load_success;
+
+    tid_t tid;
+};
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
