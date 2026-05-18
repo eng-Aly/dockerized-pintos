8dd57dad5a3d24af8e03de0bae14850803182417
AbdullahMustafa745 <abdullahmustafa45678@gmail.com>
2026-05-18 16:54:03 +0300

    new-commit

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -23,6 +23,8 @@ typedef int tid_t;
 #define PRI_MIN 0                       /* Lowest priority. */
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
+
+#define MAX_FILES 128                   //added-------------------------------------------------------------------------------------
  
 /* A kernel thread or user process.
 
